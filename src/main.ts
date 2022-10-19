0461b3290e83da4c3cc1c65b23589d91d17fadc7
Felipe Toro <fitsto@gmail.com>
2022-10-19 01:27:43 -0300

    config initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen( process.env.port || 3000);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  await app.listen(process.env.port || 3000);
 }
 bootstrap();
